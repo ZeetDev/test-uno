33cd699a371d9bf9e535fa39ef48ec8cb005f5cc
ZeetDev <zeetpatel2712@gmail.com>
2025-06-15 09:21:01 +0530

    remove old folder

--- src/app/components/Title.tsx
+++
@@ -1,5 +0,0 @@
-function Title({ children, size, color }: { children: React.ReactNode; size?: string; color?: string }) {
-    return <h1 className={`text-${size} bg-${color}-500 my_title`}>{children}</h1>;
-}
-
-export default Title;
